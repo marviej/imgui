92721ab71b893ac58557355a1a9cab8be5fc9589
Jean-Eudes Marvie <jmarvie@nvidia.com>
2025-07-18 10:32:32 +0200

    Backends: Vulkan: generate debug information in shaders SPIR-V binary

--- backends/vulkan/generate_spv.sh
+++ backends/vulkan/generate_spv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## -V: create SPIR-V binary
 ## -x: save binary output as text-based 32-bit hexadecimal numbers
+## -gVS: generate nonsemantic shader debug information with source (no performance impact)
 ## -o: output file
-glslangValidator -V -x -o glsl_shader.frag.u32 glsl_shader.frag
-glslangValidator -V -x -o glsl_shader.vert.u32 glsl_shader.vert
+glslangValidator -V -gVS -x -o glsl_shader.frag.u32 glsl_shader.frag
+glslangValidator -V -gVS -x -o glsl_shader.vert.u32 glsl_shader.vert
